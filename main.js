e2f959386e527bbacfcfe408e13a9d084bad1e2a
rumashie <massielrsanchez@gmail.com>
2024-02-15 09:04:20 -0500

    updates

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
+document.getElementById('submit_button').addEventListener('click', function() {
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+    
+    
+    console.log('Email:', email);
+    console.log('Password:', password);
+
+    //Authenticate 
+    
+});
 
