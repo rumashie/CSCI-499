ce2160074910f631de826bd4703453d741917402
Leonardo Gonzalez Luzon <leonardo.gonzalezluzon96@myhunter.cuny.edu>
2024-02-28 15:25:48 -0500

    API keys

---
+++ constants.py
@@ -0,0 +1,3 @@
+# Replace with your own OpenAI API Key https://platform.openai.com/account/api-keys
+# and rename this file to constants.py.
+APIKEY = "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"
