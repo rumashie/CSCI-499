7f50fa885cd6ebd7cc61ed92424c8090ff7e06c7
flclxo <36686123+flclxo@users.noreply.github.com>
2024-03-19 14:05:48 -0400

    Update Sidebar.js
    added symbols to this

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,15 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FaTasks, FaCalendarAlt, FaChartPie, FaUsers, FaQuestionCircle } from 'react-icons/fa'; // symbols random lib
 import './Sidebar.css';
 
 const Sidebar = () => {
   return (
     <nav className="sidebar">
       <ul>
-        <li>My tasks </li>
-        <li>Upcoming tasks</li>
-        <li>Productivity</li>
-        <li>Collaborators</li>
-        <li>Help</li>
+        <li><Link to="/"><FaTasks className="icon" /> My Tasks</Link></li>
+        <li><Link to="/"><FaCalendarAlt className="icon" /> Upcoming Tasks</Link></li>
+        <li><Link to="/StudyMode"><FaChartPie className="icon" /> Productivity</Link></li>
+        <li><Link to="/"><FaUsers className="icon" /> Collaborators</Link></li>
+        <li><Link to="/"><FaQuestionCircle className="icon" /> Help</Link></li>
       </ul>
     </nav>
   );
