16a4047ed459f19a608c53f321b96abe54a49346
Leonardo Gonzalez Luzon <leonardo.gonzalezluzon96@myhunter.cuny.edu>
2024-04-01 19:18:54 -0400

    added edit_event function

--- Chatbot.py
+++ Chatbot.py
@@ -5,7 +5,7 @@ Developed by: Leonardo Gonzalez Luzon
 
 from openai import OpenAI
 import constants
-from GoogleCalendar import create_calendar, delete_calendar, create_event, delete_event
+from GoogleCalendar import create_calendar, delete_calendar, create_event, delete_event, edit_event
 from Google import create_service
 from Spotify import get_token, play_song, create_playlist
 from weather import get_forecast
@@ -39,17 +39,19 @@ def init_messages(message_log):
     - A request for a song that can be played on Spotify
     - A request for the weather or forecast
     If you think the question is a general question that can be answered over the internet, then answer their question. 
-    If you think the question is a request to do something to their calendar, then give me a phase that corrisponds to the action they want to do. 
+    If you think the question is a request to do something to their calendar, then give me a phase that corrisponds to the action they want to do.
     Then give me the attributes needed to perform each task from the user input. 
     The attributes I need for each task goes as followed:
     Create calendar: calendar_name
     Delete calendar: calendar_name
     Create event: calendar_name, event_name, start_time, end_time, and an optional description (NOTE: start_time and end_time must contain a date and be in this format 2/25/2024 7:00am)
     Delete event: calendar_name, event_name
+    Edit event: calendar_name, event_name
 
-    IF ANY ATTRIBUTE IS MISSING PLEASE ASK THE USER FOR THE MISSING ATTRIBUTE!
+    IF ANY ATTRIBUTE IS MISSING PLEASE ASK THE USER FOR THE MISSING ATTRIBUTE UNLESS ITS OPTIONAL!
     IF THEY DON'T SPECIFY A DATE ASK THEM FOR THAT!
     MAKE SURE START AND END TIMES ARE IN THIS FORMAT 2/25/2024 7:00am NOTHING SHOULD BE LEFT OUT, YOU NEED MONTH, DAY, YEAR, AND TIME!
+    MAKE SURE THAT FOR EDIT EVENT YOU GIVE ONLY THE FUNCTION NAME, THE CALENDAR NAME, AND THE EVENT NAME. IF THE CALENDAR NAME OR EVENT NAME IS MISSING JUST SAY "I cannot process your response right now if you want to edit an event please tell me the name of your event and calendar" DONT ASK FOR MISSING INFORMATION
 
     Examples: 
 
@@ -60,10 +62,10 @@ def init_messages(message_log):
     Response: Create calendar, test calendar
 
     Question: Create an event called lunch at feb 25 2024 at 2pm for 2 hours on my test calendar
-    Response: Create event, test calendar, lunch, 2/25/2024 2:00pm, 2/25/2024 4:00pm
+    Response: Create event, test, lunch, 2/25/2024 2:00pm, 2/25/2024 4:00pm
 
     Question: Create an event called breakfast with friends at feb 25 2024 at 11pm for 4 hours on my test calendar with the description of lunch with my friends from school
-    Response: Create event, test calendar, breakfast with friends, 2/25/2024 11:00am, 2/25/2024 3:00pm, lunch with my friends from school
+    Response: Create event, test, breakfast with friends, 2/25/2024 11:00am, 2/25/2024 3:00pm, lunch with my friends from school
 
     Question: Create an event called breakfast with friends at feb 25 2024 at 11pm for 4 hours with the description of lunch with my friends from school
     Response: What calendar do you want to add it to?
@@ -76,6 +78,12 @@ def init_messages(message_log):
 
     Question: Delete lunch with friends on my test calendar
     Response: Delete event, lunch with friends, test calendar
+
+    Question: can you change my event called lunch on my college calendar to start at 1pm instead of 2pm
+    Response: Edit event, college, lunch
+
+    Question: can you change my event called study on my college calendar to be called hang out with friends
+    Response: Edit event, college, study
 
     If you think the question is a request to do something that has to do with music or something that can be accomplished with the SpotifyAPI, then give me a phrase
     that corrisponds to the action they want to do. Then give me the attributes needed to perform each task from the user input. 
@@ -130,7 +138,7 @@ def handle_message(user_input, message_log, tts_enabled=False):
     reply = chat_completion.choices[0].message.content
     message_log.append({"role": "assistant", "content": reply})
     # Process the response to determine action
-    response_text = process_response(reply)
+    response_text = process_response(reply, message_log, user_input)
     # Return a dictionary containing the chatbot's response and any action taken
     return {"response": response_text}
 
@@ -139,7 +147,7 @@ takes the chatbot response and checks if the question is to take a certain actio
 that requires the use of a calendar or Spotify function, if it does it will call 
 the function and respond with if the action was sucessful or if it was not
 """
-def process_response(reply):
+def process_response(reply, message_log, user_input):
     info = reply.split(", ")
     # Based on the action, call the appropriate function and generate a response
     if info[0] == "Create calendar":
@@ -168,6 +176,8 @@ def process_response(reply):
     
     elif info[0] == "get forecast":
         get_forecast(info[1])
+    elif info[0] == "Edit event":
+        text = edit_event(info[1], info[2], user_input, service)
     
     #if no calendar function was called then it is a general question and the answer will be printed
     else:
--- GoogleCalendar.py
+++ GoogleCalendar.py
@@ -2,11 +2,15 @@
 Developed by: Leonardo Gonzalez Luzon
 """
 
-
+from openai import OpenAI
+import constants
 from Google import convert_to_datetime
 from speech_to_text import text_to_speech, speech_to_text
 from datetime import datetime
 import pytz
+
+client = OpenAI(api_key = constants.APIKEY)
+
 """
 Creates a calendar on Google Calendar
     calendar_name: the requested name of the calendar
@@ -112,7 +116,7 @@ def get_event_ID(calendar_name, event_name, service):
     if calendarID:
         events = service.events().list(calendarId=calendarID, pageToken=page_token).execute()
         for event in events['items']:
-            if event_name == event['summary']:
+            if event_name == event.get('summary', ''):
                 return event['id']
         return None
     else:
@@ -149,7 +153,7 @@ def list_calendars(service):
 goes through the desired calendar and pulls all of the events in order of date and returns a 
 list conataing relavent information like the id title and start and end times then returns the first 5
 """
-def fetch_events(calendar_id, service, max_results=5):
+def fetch_events(calendar_id, service, max_results):
     # Set the timeMin to the current time in UTC
     now = datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
     try:
@@ -181,3 +185,102 @@ def fetch_events(calendar_id, service, max_results=5):
     except Exception as e:
         print(f"Failed to fetch events: {e}")
         return []
+
+def convert_time_format(str):
+    dt = datetime.strptime(str, '%Y-%m-%dT%H:%M:%S%z')
+    return dt.strftime('%m/%d/%Y %I:%M%p').lower()
+
+def edit_event_helper(calendar_name, user_input, service):
+    first_events = fetch_events(get_calendarID(calendar_name, service), service, 10000)
+    events = [{'title': first_event['title'], 'start': convert_time_format(first_event['start']), 'end': convert_time_format(first_event['end'])} for first_event in first_events]
+    classifier = """
+    You will recieve a list containing events in a users calendar. 
+    Each event in the list will contain 3 pieces of information pertaining to each event and those are the title, the start time, and the end time.
+    Note the start and end time is a date plus a time.
+    You will also recieve a input from the user with a request to change certain things on their event they could ask to change the name of the event, the start time, or the end time.
+    The user can ask to change up to all 3 pieces of information on the specified event
+    You will respond my giving the new information seperated by a comma, if a piece of information is not changed you will put None in its place.
+    If a user asks for a new start time but doesnt include a new date just assume its on the same day
+    If the user asks for a new start time that is after the current end time of their event and they don't give a new end time, just push the end time back based on the current event duration
+    Example: if the user's event starts at 4/2/2024 11:00am and ends at 4/2/2024 1:00pm and they say they want to change the start time to 2pm and doesnt give a end time you would return: None, 4/2/2024 2:00pm, 4/2/2024 4:00pm 
+    Make sure to respond with the changes for the correct calendar. If the user wants changes to their lunch event make sure to look at the event list to get the inforamtion connected to the event called lunch and not the information of another event
+    These are a few more examples of how you should respond 
+
+    Question: can you change lunch to start at 2pm
+    [{'title': 'lunch',
+      'start': '04/02/2024 10:45am',
+      'end': '04/02/2024 11:45am'},
+    {'title': 'dinner',
+     'start': '04/02/2024 06:00pm',
+     'end': '04/02/2024 08:00pm'}]
+    Response: None, 04/02/2024 2:00pm, 04/02/2024 3:00pm
+    
+    Question: can you change lunch to start at 11am
+    [{'title': 'lunch',
+      'start': '04/02/2024 10:45am',
+      'end': '04/02/2024 11:45am'},
+    {'title': 'dinner',
+     'start': '04/02/2024 06:00pm',
+     'end': '04/02/2024 08:00pm'}]
+    Response: None, 04/02/2024 11:00am, None
+
+    Question: can you change lunch to breakfast
+    [{'title': 'lunch',
+      'start': '04/02/2024 10:45am',
+      'end': '04/02/2024 11:45am'},
+    {'title': 'dinner',
+     'start': '04/02/2024 06:00pm',
+     'end': '04/02/2024 08:00pm'}]    
+    Response: breakfast, None, None
+
+    Question: can you change push my event called breakfast back 2 hours and call it lunch
+    [{'title': 'breakfast',
+      'start': '04/02/2024 10:45am',
+      'end': '04/02/2024 11:45am'},
+    {'title': 'dinner',
+     'start': '04/02/2024 06:00pm',
+     'end': '04/02/2024 08:00pm'}] 
+    Response: lunch, 04/02/2024 12:45pm, 04/02/2024 1:45pm
+    """
+    message_log = [{'role' : "system", "content" : classifier}]
+    events_str = ', '.join([f"{event['title']} (Start: {event['start']}, End: {event['end']})" for event in events])
+    message = f"These are my events: {events_str}. And this is my request: {user_input}"
+    message_log.append({"role": "user", "content": message})
+    chat_completion = client.chat.completions.create(
+        messages=message_log,
+        model="gpt-3.5-turbo"
+    )
+    reply = chat_completion.choices[0].message.content
+    info = reply.split(", ")
+    for i in range(3):
+        if info[i] == "None":
+            info[i] = None
+    return info[0], info[1], info[2]
+
+
+
+def edit_event(calendar_name, event_name, user_input, service):
+    new_event_name, new_start_time, new_end_time = edit_event_helper(calendar_name, user_input, service)
+
+    calendarID = get_calendarID(calendar_name, service)
+    eventID = get_event_ID(calendar_name, event_name, service)
+    if calendarID and eventID:
+        event = service.events().get(calendarId=calendarID, eventId=eventID).execute()
+        if new_event_name:
+            event['summary'] = new_event_name
+        if new_start_time:
+            event['start'] = {
+                'dateTime': convert_to_datetime(new_start_time),
+                'timeZone': 'America/New_York',
+            }
+        if new_end_time:
+            event['end'] = {
+                'dateTime': convert_to_datetime(new_end_time),
+                'timeZone': 'America/New_York',
+            }
+        service.events().update(calendarId=calendarID, eventId=eventID, body=event).execute()
+        str = "\n" + "Event updated successfully." + "\n"
+        return str
+    else:
+        str = "\n" + "Either the calendar or the event doesn't exist." + "\n"
+        return str
