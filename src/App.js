625912901e869b10d75ad4f2d04bc1574ddbb222
flclxo <36686123+flclxo@users.noreply.github.com>
2024-04-29 15:47:19 -0400

    Update App.js
    need to adjust for sending email but need to host it via db

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; 
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
 import TaskList from './components/Tasklist';
 import CalendarView from './components/CalendarView';
 import WidgetSection from './components/WidgetSection';
-import StudyMode from './components/StudyMode'; 
+import StudyMode from './components/StudyMode';
 import WeatherWidget from './components/WeatherWidget';
+import EmailSummarizer from './components/EmailSummarizer';
 
 function App() {
   return (
@@ -18,7 +19,8 @@ function App() {
           <Sidebar />
           <div className="main-content">
             <Routes>
-              <Route path="/productivity" element={<StudyMode />} /> {/* need to host this, online or some shit m please look */}
+              <Route path="/productivity" element={<StudyMode />} />
+              <Route path="/collaborators" element={<EmailSummarizer />} />
               <Route path="/" element={
                 <>
                   <TaskList />
